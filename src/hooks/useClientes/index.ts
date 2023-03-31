72fdc443db84ed27f3d804f720a15cbef1e582ce
Diego Ferreira <diferreira.program@gmail.com>
2023-03-31 17:05:15 -0300

    useCliente

--- src/hooks/useClientes/index.ts
+++ src/hooks/useClientes/index.ts
@@ -14,25 +14,19 @@ interface iUseCliente {
   isError: boolean;
   ErrorMessage: string;
   GetClientes: () => iCliente[];
-  GetCliente: (id: number) => iCliente;
-  UpdateCliente: (value: iCliente) => iCliente;
+  // GetCliente: (id: number) => iCliente;
+  // UpdateCliente: (value: iCliente) => iCliente;
 }
 
-const ROUTE_CLIENTE = '/ClientesService/Listar';
+const ROUTE_CLIENTE = '/ClientesService/';
 
 const CreateFilter = ({ filter, orderBy, skip, top }: iFilter): string => {
   let ResultRoute: string = ROUTE_CLIENTE;
   return ResultRoute;
 };
 
-const GetClientes = async ({
-  filter,
-  orderBy,
-  skip = 0,
-  top = 15,
-}: iFilter): Promise<iCliente[]> => {
-  const ROUTE = CreateFilter({ filter, orderBy, skip, top });
-  const response = await api.get(ROUTE);
+const GetClientes = (): iCliente[] => {
+  api.get(`${ROUTE_CLIENTE}Listar`).then((response) => {});
   return response.data.Dados;
 };
 
@@ -47,6 +41,24 @@ const GetClientes = async ({
 //   return response.data;
 // };
 
-const useClientes = create<iUseCliente>((set) => ({}));
+const useClientes = create<iUseCliente>((set) => ({
+  ErrorMessage: '',
+  isError: false,
+  isLoading: false,
+  GetClientes: () => {
+    set(state => ({
+      let data: iCliente[] = [];
+    api
+      .get(`${ROUTE_CLIENTE}Listar`)
+      .then(async (response) => {
+        data = await response.data.Dados;
+      })
+      .catch(error => {
+        
+      })
+      .finally();
+    }))
+  },
+}));
 
 export default useClientes;
